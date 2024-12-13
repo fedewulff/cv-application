5c3340d5e03e8b2ed58f70c0718f72981b314577
Fede Wulff <federicowulff95@gmail.com>
2024-12-13 16:30:26 -0300

    function to hide form and and edit them was added

--- src/App.jsx
+++ src/App.jsx
@@ -52,9 +52,35 @@ function App() {
     setDateIn(``);
     setDateOut(``);
   }
-  function buttonPressed(parameter) {
-    const form = document.querySelector(parameter);
-    form.classList.add(`hideform`);
+  function hideForm(ogform, secondform, thirdform) {
+    const form = document.querySelector(ogform);
+    const otherform1 = document.querySelector(secondform);
+    const otherform2 = document.querySelector(thirdform);
+
+    if (form.classList.contains("hideform")) {
+      form.classList.remove(`hideform`);
+      otherform1.classList.add(`hideform`);
+      otherform2.classList.add(`hideform`);
+    } else {
+      form.classList.add(`hideform`);
+    }
+  }
+  function editForm1(parameter) {
+    setName(parameter.name);
+    setPhoneNumber(parameter.phoneNumber);
+    setEmail(parameter.email);
+  }
+  function editForm2(parameter) {
+    setplaceOfStudy(parameter.placeOfStudy);
+    setDegree(parameter.degree);
+    setDate(parameter.date);
+  }
+  function editForm3(parameter) {
+    setCompanyName(parameter.companyName);
+    setPositionTitle(parameter.positionTitle);
+    setResponsibilities(parameter.responsibilities);
+    setDateIn(parameter.dateIn);
+    setDateOut(parameter.dateOut);
   }
   return (
     <>
@@ -63,85 +89,142 @@ function App() {
           <div className="firstform">
             <div className="title">
               <h1 className="h1style">Me</h1>
-              <button onClick={() => buttonPressed(`.form1`)} className="botoncito">
+              <button onClick={() => hideForm(`.form1`, `.form2`, `.form3`)} className="botoncito">
                 {" "}
                 &gt;{" "}
               </button>
             </div>
             <form className="form form1" onSubmit={handleSubmitMyInfo}>
               <label htmlFor="name">
-                Name: <input value={name} type="text" id="name" onChange={(e) => setName(e.target.value)} required />
+                Name: <input value={name} autoComplete="off" type="text" id="name" onChange={(e) => setName(e.target.value)} required />
               </label>
               <label htmlFor="phone">
                 Phone number:{" "}
-                <input value={phoneNumber} type="number" id="phone" onChange={(e) => setPhoneNumber(e.target.value)} required />
+                <input
+                  value={phoneNumber}
+                  autoComplete="off"
+                  type="number"
+                  id="phone"
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                  required
+                />
               </label>
               <label htmlFor="email">
-                E-mail: <input value={email} type="email" id="email" onChange={(e) => setEmail(e.target.value)} required />
+                E-mail:{" "}
+                <input value={email} autoComplete="off" type="email" id="email" onChange={(e) => setEmail(e.target.value)} required />
               </label>
-              <button className="formbuttons" type="submit">
-                SEND
-              </button>
+              <div className="buttons">
+                <button className="formbuttons" type="button" onClick={() => editForm1(myInfo)}>
+                  EDIT
+                </button>
+                <button className="formbuttons" type="submit">
+                  SEND
+                </button>
+              </div>
             </form>
           </div>
 
           <div className="secondform">
             <div className="title">
               <h1 className="h1style">Education</h1>
-              <button onClick={() => buttonPressed(`.form2`)}> &gt; </button>
+              <button onClick={() => hideForm(`.form2`, `.form1`, `.form3`)}> &gt; </button>
             </div>
-            <form className="form form2" onSubmit={handleSubmitMyEducation}>
+            <form className="form form2 hideform" onSubmit={handleSubmitMyEducation}>
               <label htmlFor="placeofstudy">
                 Place of study:{" "}
-                <input value={placeOfStudy} type="text" id="placeofstudy" onChange={(e) => setplaceOfStudy(e.target.value)} required />
+                <input
+                  value={placeOfStudy}
+                  autoComplete="off"
+                  type="text"
+                  id="placeofstudy"
+                  onChange={(e) => setplaceOfStudy(e.target.value)}
+                  required
+                />
               </label>
               <label htmlFor="degree">
-                Degree: <input value={degree} type="text" id="degree" onChange={(e) => setDegree(e.target.value)} required />
+                Degree:{" "}
+                <input value={degree} autoComplete="off" type="text" id="degree" onChange={(e) => setDegree(e.target.value)} required />
               </label>
               <label htmlFor="date">
-                Date of graduation: <input value={date} type="date" id="date" onChange={(e) => setDate(e.target.value)} required />
+                Date of graduation:{" "}
+                <input value={date} autoComplete="off" type="date" id="date" onChange={(e) => setDate(e.target.value)} required />
               </label>
-              <button className="formbuttons" type="submit">
-                SEND
-              </button>
+              <div className="buttons">
+                <button className="formbuttons" type="button" onClick={() => editForm2(myEducation)}>
+                  EDIT
+                </button>
+                <button className="formbuttons" type="submit">
+                  SEND
+                </button>
+              </div>
             </form>
           </div>
 
           <div className="thirdform">
             <div className="title">
               <h1 className="h1style">Last job</h1>
-              <button onClick={() => buttonPressed(`.form3`)}> &gt; </button>
+              <button onClick={() => hideForm(`.form3`, `.form1`, `.form2`)}> &gt; </button>
             </div>
-            <form className="form form3" onSubmit={handleSubmitMyLastJob}>
+            <form className="form form3 hideform" onSubmit={handleSubmitMyLastJob}>
               <label htmlFor="companyname">
                 Company name:{" "}
-                <input value={companyName} type="text" id="companyname" onChange={(e) => setCompanyName(e.target.value)} required />
+                <input
+                  value={companyName}
+                  autoComplete="off"
+                  type="text"
+                  id="companyname"
+                  onChange={(e) => setCompanyName(e.target.value)}
+                  required
+                />
               </label>
               <label htmlFor="positiontitile">
                 Position title:{" "}
-                <input value={positionTitle} type="text" id="positiontitile" onChange={(e) => setPositionTitle(e.target.value)} required />
+                <input
+                  value={positionTitle}
+                  autoComplete="off"
+                  type="text"
+                  id="positiontitile"
+                  onChange={(e) => setPositionTitle(e.target.value)}
+                  required
+                />
               </label>
               <label htmlFor="responsibilities">
                 Responibilities:{" "}
-                <input
+                <textarea
                   value={responsibilities}
+                  autoComplete="off"
                   type="text"
                   id="responsibilities"
+                  rows={5}
                   onChange={(e) => setResponsibilities(e.target.value)}
                   required
                 />
               </label>
               <div className="dateinandout">
                 <label htmlFor="datein">
-                  Started on: <input value={dateIn} type="date" id="dateina" onChange={(e) => setDateIn(e.target.value)} required />
+                  Started on:{" "}
+                  <input value={dateIn} autoComplete="off" type="date" id="datein" onChange={(e) => setDateIn(e.target.value)} required />
                 </label>
                 <label htmlFor="dateout">
-                  Left on: <input value={dateOut} type="date" id="dateout" onChange={(e) => setDateOut(e.target.value)} required />
+                  Left on:{" "}
+                  <input
+                    value={dateOut}
+                    autoComplete="off"
+                    type="date"
+                    id="dateout"
+                    onChange={(e) => setDateOut(e.target.value)}
+                    required
+                  />
                 </label>
               </div>
-              <button className="formbuttons" type="submit">
-                SEND
-              </button>
+              <div className="buttons">
+                <button className="formbuttons" type="button" onClick={() => editForm3(myLastJob)}>
+                  EDIT
+                </button>
+                <button className="formbuttons" type="submit">
+                  SEND
+                </button>
+              </div>
             </form>
           </div>
         </div>
